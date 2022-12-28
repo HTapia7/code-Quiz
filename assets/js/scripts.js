a42803b3d34faa035d6d71abfecb6673d99cffb0
Hernan Tapia <soliditize@MacBook-Pro.local>
2022-12-27 22:27:28 -0800

    added questions

---
+++ assets/js/scripts.js
@@ -0,0 +1,59 @@
+var questions = [
+    {
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+        "Hyper Text Preprocessor",
+        "Hyper Text Markup Language",
+        "Hyper Text Multiple Language",
+        "Hyper Tool Multi Language"
+    ]
+    },
+    {
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+        "Common Style Sheet",
+        "Colorful Style Sheet",
+        "Computer Style Sheet",
+        "Cascading Style Sheet"
+    ]
+    },
+    {
+
+    question: "Which do you use for an array ?",
+    answer: "brackets",
+    options: [
+        "commas",
+        "paranthesis",
+        "curly braces",
+        "brackets"
+    ]
+    },
+    {
+
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+        "Stylish Question Language",
+        "Stylesheet Query Language",
+        "Statement Question Language",
+        "Structured Query Language"
+    ]
+    },
+    {
+
+    question: "Strings can be stored in _____ ?",
+    answer: "quotes",
+    options: [
+        "commas",
+        "paranthesis",
+        "curly braces",
+        "quotes"
+    ]
+    }]
+
+var startButton = document.getElementById('start-btn')
+var nextButton = document.getElementById('next-btn')
+
+
